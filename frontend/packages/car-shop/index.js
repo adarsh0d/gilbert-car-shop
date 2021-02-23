eba3a6e773bec66759b96a9e0f403ac31c283733
Adarsh Somani <adarshsomani64@gmail.com>
2021-02-23 23:02:36 +0100

    test: added unit test cases for car details popup

--- frontend/packages/car-shop/index.js
+++ frontend/packages/car-shop/index.js
@@ -1,3 +1,4 @@
 export { CarShop } from './src/CarShop.js';
 export { CarsList } from './src/CarsList.js';
 export { CarCard } from './src/CarCard.js';
+export { CarDetails } from './src/CarDetails.js';
